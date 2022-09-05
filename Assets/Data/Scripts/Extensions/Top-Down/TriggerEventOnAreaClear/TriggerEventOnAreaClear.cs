10185c5f12ae7af1113b203a7fb555a04f6bbd46
Chance Cornell <chancecornell@gmail.com>
2022-09-04 22:21:05 -0500

    Added doors that open when all enemies are destroyed in a room.

---
+++ Assets/Data/Scripts/Extensions/Top-Down/TriggerEventOnAreaClear/TriggerEventOnAreaClear.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace TopDownEngineExtensions
+{
+    /// <summary>
+    /// Requires a Collider and objects with the AreaClearTarget component inside said collider
+    /// </summary>
+    public class TriggerEventOnAreaClear : MonoBehaviour
+    {
+        [Tooltip("the event to trigger when the area is cleared (all objects marked with AreaClearTarget have been destroyed)")]
+        public UnityEvent OnAreaCleared;
+        private int _numberOfTargetsAlive;
+
+        public void AddTarget() { _numberOfTargetsAlive++; }
+
+        public void RemoveTarget()
+        {
+            _numberOfTargetsAlive--;
+            if (_numberOfTargetsAlive == 0) OnAreaCleared?.Invoke();
+        }
+    }
+}
