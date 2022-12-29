0774e380e0abc7c5d49f9d3b9edd56be6e5c8d16
Chance Cornell <chancecornell@gmail.com>
2022-12-29 12:58:03 -0600

    Update 12/29/22
    :sparkles: Added sliders for control of each audio track

---
+++ Assets/MMSoundManagerUtility/MMSoundManagerUtility.cs
@@ -0,0 +1,18 @@
+﻿using MoreMountains.Tools;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MMSoundManagerUtility")]
+public class MMSoundManagerUtility : ScriptableObject
+{
+    public void SetVolumeMaster(float newVolume) => MMSoundManagerTrackEvent.Trigger(
+    MMSoundManagerTrackEventTypes.SetVolumeTrack, MMSoundManager.MMSoundManagerTracks.Master, newVolume);
+
+    public void SetVolumeMusic(float newVolume) => MMSoundManagerTrackEvent.Trigger(
+        MMSoundManagerTrackEventTypes.SetVolumeTrack, MMSoundManager.MMSoundManagerTracks.Music, newVolume);
+
+    public void SetVolumeSfx(float newVolume) => MMSoundManagerTrackEvent.Trigger(
+    MMSoundManagerTrackEventTypes.SetVolumeTrack, MMSoundManager.MMSoundManagerTracks.Sfx, newVolume);
+
+    public void SetVolumeUi(float newVolume) => MMSoundManagerTrackEvent.Trigger(
+    MMSoundManagerTrackEventTypes.SetVolumeTrack, MMSoundManager.MMSoundManagerTracks.UI, newVolume);
+}
